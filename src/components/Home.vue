ca89453350a9081321d482a58d1424e14aba261e
chen <184913704@qq.com>
2020-02-20 18:08:23 +0800

    登录登出

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    <el-button type="info" @click="loginout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+    loginout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
+
+<style scoped lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
+import Login from "../components/Login.vue";
+import Home from "../components/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "/login"
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    component: Login
+  },
+  {
+    path: "/home",
+    component: Home
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path == "/login") return next();
+  const Token = window.sessionStorage.getItem("token");
+  if (!Token) return next("/login");
+  next();
 });
 
 export default router;
